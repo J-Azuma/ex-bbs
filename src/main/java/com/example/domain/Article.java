396a3071ed94e36273c179a1ebba7346f7f1fc73
J-azuma <azumanotdetail@gmail.com>
2020-06-24 13:46:41 +0900

    create ArticleRepository

--- src/main/java/com/example/domain/Article.java
+++ src/main/java/com/example/domain/Article.java
@@ -1,5 +1,36 @@
 package com.example.domain;
 
+/**
+ * 記事情報を保持するドメインクラス.
+ * 
+ * @author junpei.azuma
+ *
+ */
 public class Article {
-
+	private Integer id;
+	private String name;
+	private String content;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + "]";
+	}
+	
 }
---
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -0,0 +1,66 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
+
+/**
+ * 記事の操作を行うリポジトリクラス.
+ * 
+ * @author junpei.azuma
+ *
+ */
+@Repository
+public class ArticleRepository {
+	@Autowired 
+	private NamedParameterJdbcTemplate template;
+	
+	private RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+		Article article = new Article();
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+		return article;
+	};
+	
+	/**
+	 * 記事の全件検索を行う.
+	 * 
+	 * @return 全ての記事のリスト
+	 */
+	public  List<Article> findAll() {
+		String sql = "select id, name, content from articles order by id desc;";
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+		return articleList;
+	}
+	
+	/**
+	 * 記事の作成を行う.
+	 * 
+	 * @param article 作成する記事
+	 */
+	public void insert(Article article) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(article);
+		String sql = "insert into article(name, content) values(:name, :content);";
+		template.update(sql, param);
+	}
+	
+	/**
+	 * 記事の削除を行う.
+	 * 
+	 * @param id 削除対象の記事id
+	 */
+	public void deleteById(int id) {
+		String sql = "delete from articles where id = :id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		template.update(sql, param);
+	}
+}
